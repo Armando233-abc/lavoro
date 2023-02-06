65f55b95d31c6bd48f838c5512474aefe644fb8d
Armando233-abc <armandovigliotti5@gmail.com>
2023-02-06 16:35:58 +0100

    foto

--- foto.js
+++ foto.js
@@ -1,9 +1,8 @@
-let camera_button = document.querySelector("#start-camera");
 let video = document.querySelector("#video");
 let click_button = document.querySelector("#click-photo");
 let canvas = document.querySelector("#canvas");
 
-camera_button.addEventListener('click', async function () {
+async function mostra_camera() {
       let stream = await navigator.mediaDevices.getUserMedia({ video: { 
             width: {
                   min: 1280,
@@ -17,9 +16,18 @@ camera_button.addEventListener('click', async function () {
             },
             facingMode:"environment"}, audio: false });
       video.srcObject = stream;
-});
+}
+mostra_camera()
 
 click_button.addEventListener('click', function () {
       canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-      
+      video.srcObject = null;
+      video.style.display = 'none';
+      click_button.dysplay = 'none';
+
+      const contenitore = document.querySelector('#contenitore');
+      contenitore.style.display = 'none';
+
+      const contenitore_img = document.querySelector('#contenitore_img');
+      contenitore_img.style.display = 'block';
 });
